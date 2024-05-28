53d0557ab76be321b9e12eb1930be1916c926eaa
Chaitany Arora <arorachaitany15@gmail.com>
2024-05-28 12:08:28 +0530

    OfficePc: Env variables set up and database connection

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -1,14 +1,18 @@
+import { Link } from "react-router-dom";
 
 export default function LoginPage() {
     return (
         <div className="mt-4 grow flex items-center justify-around">
             <div className="mb-64">
-            <h1 className="text-4xl text-center mb-4">Login</h1>
-           <form className="max-w-md mx-auto">
-            <input type="email" placeholder="Enter your email" />
-            <input type="password" placeholder="Enter your password" />
-            <button className="primary">Login</button>
-           </form>
+                <h1 className="text-4xl text-center mb-4">Login</h1>
+                <form className="max-w-md mx-auto">
+                    <input type="email" placeholder="Enter your email" />
+                    <input type="password" placeholder="Enter your password" />
+                    <button className="primary">Login</button>
+                    <div className="text-center py-2 text-gray-500">
+                        Don't have an account? <Link className="hover:underline text-black" to={'/register'}>Register Now</Link>
+                    </div>
+                </form>
             </div>
         </div>
     )
